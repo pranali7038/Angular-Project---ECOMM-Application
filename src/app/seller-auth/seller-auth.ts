6481e6d00c680b5440288b24ee5eeb01755035c2
Pranali Jahagirdar <jahagirdarpranali3409@gmail.com>
2025-09-11 13:01:24 +0530

    seller signup form

--- src/app/seller-auth/seller-auth.ts
+++ src/app/seller-auth/seller-auth.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-auth',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './seller-auth.html',
   styleUrls: ['./seller-auth.css']   
 })
-export class SellerAuth {}
+export class SellerAuth {
+
+  signUp(data:object):void{
+    console.log(data);
+    
+  }
+}
