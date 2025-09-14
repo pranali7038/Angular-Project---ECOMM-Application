9d9e0fd1506b32b189a00839d8afb891c4c01c05
Pranali Jahagirdar <jahagirdarpranali3409@gmail.com>
2025-09-14 15:46:47 +0530

    add product form and API

--- src/app/seller-add-product/seller-add-product.ts
+++ src/app/seller-add-product/seller-add-product.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-seller-add-product',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './seller-add-product.html',
   styleUrl: './seller-add-product.css'
 })
 export class SellerAddProduct {
 
+    constructor(){}
+
+    ngOnInit():void{
+
+    }
+    submit(data:object){
+      console.log(data);
+      
+    }
 }
