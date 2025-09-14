137639ed4d7e51dde1683ab4c0ca93d9a03da4fc
Pranali Jahagirdar <jahagirdarpranali3409@gmail.com>
2025-09-14 14:02:39 +0530

    menu changed after seller login

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule} from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { RouterLink,Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  menuType:string = "default"
+  constructor(private route:Router){}
+
+  ngOnInit():void{
+    this.route.events.subscribe((val:any)=>{
+      if(val.url){
+        
+        if(localStorage.getItem('seller') && val.url.includes('seller')){
+          console.log("this is seller area");
+          this.menuType = "seller";
+          
+        }else{
+          this.menuType ="default"
+          
+        }
+      } 
+      
+    })
+  }
 
 }
--- src/app/services/seller.ts
+++ src/app/services/seller.ts
@@ -2,7 +2,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Login, SignUp } from '../data-type';
 import { BehaviorSubject } from 'rxjs';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -12,7 +12,7 @@ export class Seller {
   isSellerLoggedIn = new BehaviorSubject<boolean>(false);
   isLoginError = new EventEmitter<boolean>(false);
 
-  constructor(private http: HttpClient,private router:Router) {}
+  constructor(private http: HttpClient, private router: Router) { }
 
   userSignUp(data: SignUp) {
     this.http.post('http://localhost:3000/seller', data, { observe: 'response' })
@@ -20,35 +20,41 @@ export class Seller {
         console.log(result);
         if (result) {
           this.isSellerLoggedIn.next(true);
-          localStorage.setItem('seller',JSON.stringify(result.body)); 
-          this.router.navigate(['seller-home']); 
+          localStorage.setItem('seller', JSON.stringify(result.body));
+          this.router.navigate(['seller-home']);
         }
       });
   }
 
-  reloadSeller(){
-    if(localStorage.getItem('seller')){
+  reloadSeller() {
+    if (localStorage.getItem('seller')) {
       this.isSellerLoggedIn.next(true);
-      this.router.navigate(['seller-home']) 
+      this.router.navigate(['seller-home'])
     }
   }
 
-  userLogin(data:Login){
-    
-    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`,{
-      observe:'response'}).subscribe((result:any)=>{
-        console.log(result);
-        
-        if(result && result.body && result.body.length===1){
-           this.isLoginError.emit(false);
-          localStorage.setItem('seller',JSON.stringify(result.body[0])); 
-          this.router.navigate(['seller-home']); 
-        }else{
-          this.isLoginError.emit(true);
-          
-        }
-      })
-    
+  userLogin(data: Login) {
+
+    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`, {
+      observe: 'response'
+    }).subscribe((result: any) => {
+      console.log(result);
+
+      if (result && result.body && result.body.length > 0) {
+        //  this.isLoginError.emit(false);
+        // localStorage.setItem('seller',JSON.stringify(result.body[0])); 
+        // this.router.navigate(['seller-home']); 
+        const seller = result.body[0].data ? result.body[0].data : result.body[0];
+
+        this.isLoginError.emit(false);
+        localStorage.setItem('seller', JSON.stringify(seller));
+        this.router.navigate(['seller-home']);
+      } else {
+        this.isLoginError.emit(true);
+
+      }
+    })
+
   }
 
 }
